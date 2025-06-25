1a02fe0f39f0d0a787d62546050c8f8b30acdfd3
Hristo Ivanov <105433871+hristogwivanov@users.noreply.github.com>
2025-06-25 11:11:44 +0300

    Presentation sandbox created

---
+++ 01.Intro to Angular and TypeScript/presentation.ts
@@ -0,0 +1,67 @@
+//HTTP Basics
+
+//HTTP Server - Client
+//Hyper Text Transfer Protocol(HTTP)
+// -Client-server protocol for transferring Web resources
+//Important properties of HTTP
+// -Request-response model
+// -Text-based format
+// -Relise on a unique resource URLs
+// -Provides resource metadata (e.g., encoding
+// -Stateless (cookies can overcome this)\
+
+//HTTP: Request-Response Protocol
+// -Client program
+//  -Running on the end host.
+//  -E.g. Web browser
+//  -Request a resource
+// -Server program
+//  -Running at the server
+//  -E.g. Web server
+//  -Provides resources 
+
+//Example
+//GET /index.html
+//HTTP/1.0
+
+//HTTP/1.0 200 OK
+//"Welcome to our Web Site!"
+
+//Example: Hyper Text Transfer Protocol
+//HTTP request
+//GET /courses/about.aspx HTTP/1.1
+//Host: www.softuni.com
+//User-Agent: Mozilla/5.0
+//<CRLF>
+//HTTP response
+//HTTP/1.1 200 OK
+//Date: Mon, 5 Jul 2010 13:09:03 GMT
+//Server: Microsoft-HTTPAPI/2.0
+//Last-Modified; Mon, 12 Jul 2010 15:33:23 GMT
+//Content-Length: 54
+//<CRLF> 
+//<html><title>Hello</title>Welcome to our site</html>
+
+//Routing Overview
+//Navigation for Single Page Apps
+//What is Routing? 
+// -Allows navigation, without reloading the page
+// -Pivotal element of writing Single Page Applications
+
+//Single Page Applications
+// -A Router loads the appropriate content when the location changes
+//  -E.g. when the user manually enters an address
+// -Converseley, a change in content is reflected 
+//  -E.g. when the user clicks on a link
+// -Benefits
+//  -Load all scripts only once
+//  -Maintain state across multiple pages
+//  -Browser history can be used
+//  -Build User Interfaces that react quickly
+
+
+//Angular Overview
+//Web Application Platform
+//What is Angular
+// -Angular is a platform for building complex front-end apps
+// -Focused on end-to-end tooling and best practices
