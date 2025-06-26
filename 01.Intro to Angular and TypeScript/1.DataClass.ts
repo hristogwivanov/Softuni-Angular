353d62b4f9d45495abaa778339afb2d238735c2b
Hristo Ivanov <105433871+hristogwivanov@users.noreply.github.com>
2025-06-26 09:18:11 +0300

    Task1 complete

--- 01.Intro to Angular and TypeScript/1.DataClass.ts
+++ 01.Intro to Angular and TypeScript/1.DataClass.ts
@@ -1,6 +1,20 @@
 class Data {
-    constructor () => {
+    public method: string;
+    public uri: string;
+    public version: string;
+    public message: string;
+    public response: string | undefined;
+    public fulfilled: boolean;
 
+    constructor (method: string, uri: string, version: string, message: string, response?: string, fulfilled: boolean = false){
+        this.method = method;
+        this.uri = uri;
+        this.version = version;
+        this.message = message;
+        this.response = response;
+        this.fulfilled = false;
     }
-
 }
+
+let myData = new Data ('GET', 'http://google.com', 'HTTP/1.1', '');
+console.log(myData);
