bb36e18fedbb7bcd13f2a12f11670a03e2b530f7
Hristo Ivanov <105433871+hristogwivanov@users.noreply.github.com>
2025-07-09 11:55:56 +0300

    ArticleComponent class created

--- 02.Components/Angular Components Lab/articles-app/src/app/article/article.ts
+++ 02.Components/Angular Components Lab/articles-app/src/app/article/article.ts
@@ -9,3 +9,19 @@ import { Component } from '@angular/core';
 export class Article {
 
 }
+
+export class ArticleComponent{
+  private symbols: number = 250; 
+  @Input() article: Article; 
+  @input() articleDesc: string;
+  descToShow : string;
+  articleDescLen : number;
+  showReadMoreBtn : boolean = true;
+  showHideBtn : boolean = false;
+  imageIsShown: boolean = false;
+  imageButtonTitle: string = "Show Image";
+  constructor() {
+    this.articleDescLen = 0;
+    this.descToShow = "";
+  }
+}
